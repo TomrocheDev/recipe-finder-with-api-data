59f210dfef0d3e1b1130bf481aac4c90230dfd4c
Tom Roche <tomroche.development@gmail.com>
2023-05-27 01:05:48 +0200

    Optimize recipe rendering in saved recipes.

--- static/scripts/saved-recipes.js
+++ static/scripts/saved-recipes.js
@@ -9,11 +9,10 @@ let savedRecipesLS = JSON.parse(localStorage["savedRecipes"]) || [];
 savedRecipesLS.forEach((element) => {
   let newCard = document.createElement("div");
   newCard.classList.add("card");
-  newCard.setAttribute("style", "width: 18rem");
+  newCard.setAttribute("style", "width: 18rem;");
 
   newCard.innerHTML = `
-    <div class="card" style="width: 18rem;">
-        <img src="${element.image}" class="card-img-top" alt="...">
+        <img src="${element.image}" class="card-img-top">
         <div class="card-body">
             <h5 class="card-title">${element.title}</h5>
             <p class="card-text">
@@ -21,9 +20,10 @@ savedRecipesLS.forEach((element) => {
                 <span class="servings">Servings: ${element.servings}</span><br />
                 <span class="source">Source: ${element.source}</span><br />
             </p>
-            <a href="${element.link}" class="btn btn-danger">Visit site</a>
-        </div>
-    </div>`;
+            <div class="card-buttons">
+                <a href="${element.link}" class="btn btn-outline-danger visit-btn" target="_blank">Visit site</a>
+                <button class="btn btn-danger delete-btn">Delete recipe</button>
+        </div>`;
 
   savedRecipesContainer.append(newCard);
 });
