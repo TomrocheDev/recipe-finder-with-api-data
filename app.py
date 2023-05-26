9fbe6ffefe6b01c43a853d66fa913ec07d5b3bae
Tom Roche <tomroche.development@gmail.com>
2023-05-26 19:19:28 +0200

    Make displaying next pages possible.

--- app.py
+++ app.py
@@ -1,15 +1,34 @@
-from flask import Flask, request, render_template, redirect, url_for, session
-from functions.functions import get_recipes
+from flask import Flask, request, render_template, session
+import requests
+import os
 
 app = Flask(__name__)
+app.secret_key = "123456"
 
+app_id = os.environ["EDAMAM-APP-ID"]
+api_key = os.environ["EDAMAM-API-KEY"]
 
 @app.route("/", methods=["GET", "POST"])
-def home():
+def index():
     data = None
     if request.method == "POST":
-        user_input = request.form["user-input"]
-        data = get_recipes(user_input)
+        session["input"] = request.form["user-input"]
+        response = requests.get(f"https://api.edamam.com/api/recipes/v2?type=public&q={session['input']}&app_id={app_id}"
+                                f"&app_key={api_key}").json()
+        data = response["hits"]
+        session["next"] = response["_links"]["next"]["href"]
+
+    return render_template("index.html", recipes=data)
+
+
+@app.route("/next_page")
+def next_page():
+    next_page_url = session["next"]
+    new_response = requests.get(next_page_url).json()
+
+    data = new_response["hits"]
+    session["next"] = new_response["_links"]["next"]["href"]
+
     return render_template("index.html", recipes=data)
 
 
